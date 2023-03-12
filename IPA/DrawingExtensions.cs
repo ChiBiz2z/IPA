b79bb1e9b3b907db74ed06d64e6d5d97868ee75b
Vladimir <vvbedash@gmail.com>
2023-03-12 19:39:59 +0600

    Добавлены те методы, которые добавлены

---
+++ IPA/DrawingExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace IPA;
+
+public static class DrawingExtensions
+{
+    /// <summary>
+    /// For binary images
+    /// </summary>
+    public static bool IsForeground(this Color color) => color is { R: >= 245, G: >= 245, B: >= 245 };
+}
